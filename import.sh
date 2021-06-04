95154a1849737fb1c03f34c37b81b6e44cc9ed77
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2021-06-04 17:53:33 +0100

    switch to pull in import.sh

--- import.sh
+++ import.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec git subtree add --squash -P packages/"$1" "$2" "$3" -m "Import $2 $3 as packages/$1"
+exec git subtree pull --squash -P packages/"$1" "$2" "$3" -m "Import $2 $3 as packages/$1"
